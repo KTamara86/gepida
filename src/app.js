7a13274f716c4b9e1c4ca938a1b49c1e494553cb
Kukta Tamara <kukta.tamara@gmail.com>
2023-03-11 14:29:05 +0100

    First commit

---
+++ src/app.js
@@ -0,0 +1,38 @@
+const arlistaTorzs = document.querySelector('#arlistaTorzs');
+const nevInput = document.querySelector('#nev');
+const kerekInput = document.querySelector('#kerek');
+const hasznalatInput = document.querySelector('#hasznalat');
+const arInput = document.querySelector('#ar');
+
+const arlistaLista = [
+    {nev: "Cassis", kerek: 28, hasznalat: "offroad", ar: 557900},
+    {nev: "Albion 900", kerek: 28, hasznalat: "trekking", ar: 519900},
+    {nev: "Asgard", kerek: 29, hasznalat: "technikás utak", ar: 519900},
+    {nev: "Ruga", kerek: 29, hasznalat: "hegyi" , ar: 372900},
+    {nev: "Reptila", kerek: 28, hasznalat: "városi", ar: 308900},
+    {nev: "Sirmium", kerek: 29, hasznalat: "hegyi", ar: 264900},
+];
+
+function loadArlista () {
+    arlistaLista.forEach((kerekpar) => {
+        let tr = document.createElement('tr');
+        let tdNev = document.createElement('td');
+        let tdKerek = document.createElement('td');
+        let tdHasznalat = document.createElement('td');
+        let tdAr = document.createElement('td');
+
+        tdNev.textContent = kerekpar.nev;
+        tdKerek.textContent = kerekpar.kerek;
+        tdHasznalat.textContent = kerekpar.hasznalat;
+        tdAr.textContent = kerekpar.ar;
+        
+        arlistaTorzs.append(tr);
+        tr.append(tdNev);
+        tr.append(tdKerek);
+        tr.append(tdHasznalat);
+        tr.append(tdAr);
+
+    });
+}
+
+loadArlista();
